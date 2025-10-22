160ab0a40fd1936ff09bed56f825b3181aa35b58
yh1567 <ikeda.yuki@nttd-mse.com>
2025-10-22 18:04:29 +0900

    ログ出力メソッド追加
    Signed-off-by: yh1567 <ikeda.yuki@nttd-mse.com>

--- tools/log_util.py
+++ tools/log_util.py
@@ -14,3 +14,27 @@ def log_method_call(func):
         logging.info(f"[IN]{func.__name__}() 引数={args}")
         return func(*args)
     return wrapper
+
+# 使用例
+'''
+①
+from log_util import log_method_callをファイルの頭に付ける
+②
+メソッドの上に@log_method_callを付ける
+
+例：
+from log_util import log_method_call
+
+class PcAdapter:
+        monitors_info = None
+        screenshotFlag = True
+        @log_method_call
+        def screenshotPrepare(self):
+                PcAdapter.monitors_info = get_monitors()
+                if PcAdapter.monitors_info is None:
+                        print("screenshotPrepare() error : monitors_info is null")
+                        return False
+                else:
+                        print(PcAdapter.monitors_info)
+                        return True
+'''
